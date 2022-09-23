136c6b8e259cfab8ca29afae6a56a3dd71ba6506
NachX90 <nachx90@protonmail.com>
2022-09-23 16:43:43 -0300

    batman

---
+++ ManejoCuentasBancarias/Cuenta.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+public enum TipoDeExtraccion
+{
+    CajeroHumano = 1,
+    CajeroAutomatico = 2
+}
+
+public class Cuenta
+{
+    public decimal saldo;
+    public TipoDeExtraccion tipo;
+
+    public bool insercion(decimal monto)
+    {
+        saldo += monto;
+        return true;
+    }
+
+    bool extraccion(TipoDeExtraccion tipo, decimal monto)
+    {
+        return false;
+    }
+
+    public class CuentaCorrientePesos
+    {
+        bool extraccion(TipoDeExtraccion tipo, decimal monto)
+        {
+            decimal saldoDespuesDeLaOperacion = saldo - monto;
+            if (saldoDespuesDeLaOperacion >= -5000)
+            {
+                saldo -= monto;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    public class CuentaCorrienteDolares
+    {
+        bool extraccion(TipoDeExtraccion tipo, decimal monto)
+        {
+            decimal saldoDespuesDeLaOperacion = saldo - monto;
+            if (saldoDespuesDeLaOperacion >= 0)
+            {
+                saldo -= monto;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    public class CajaAhorroPesos
+    {
+        bool extraccion(TipoDeExtraccion tipo, decimal monto)
+        {
+            decimal saldoDespuesDeLaOperacion = saldo - monto;
+            if (saldoDespuesDeLaOperacion >= 0)
+            {
+                saldo -= monto;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
